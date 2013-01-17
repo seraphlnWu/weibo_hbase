8536f1e0453227690683a1e0d25a4c50aff3620b
wubin <wubin@admaster.com.cn>
2013-01-17 18:29:11 +0800

    add two funcs for get user from hbase

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -1,3 +1,19 @@
 # coding=utf8
 
+from utils import MONGODB_INSTANCE
+from utils import HBASE_INSTANCE
 
+USER_TABLE = HBASE_INSTANCE.table('users')
+
+def get_users():
+    '''
+        获取全部的用户信息列表    
+    '''
+    return USER_TABLE.scan()
+
+
+def get_user_by_id(uid):
+    '''
+        根据传入的uid获取相应的user信息
+    '''
+    return USER_TABLE.row(str(uid))
