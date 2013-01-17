d36eb9406d7c4490d1f401aab9e18daa3c544092
wubin <wubin@admaster.com.cn>
2013-01-17 18:45:35 +0800

    add parser in user_dao

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -3,6 +3,8 @@
 from utils import MONGODB_INSTANCE
 from utils import HBASE_INSTANCE
 
+from weibo_dao.parser.parser import ModelParser
+
 USER_TABLE = HBASE_INSTANCE.table('users')
 
 def get_users():
@@ -16,4 +18,5 @@ def get_user_by_id(uid):
     '''
         根据传入的uid获取相应的user信息
     '''
-    return USER_TABLE.row(str(uid))
+    mp = ModelParser()
+    return mp.parse('users', USER_TABLE.row(str(uid)))
