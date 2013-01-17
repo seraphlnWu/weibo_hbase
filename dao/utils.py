b190f52bc21b66f5a2bf9247206ad628f72777f7
wubin <wubin@admaster.com.cn>
2013-01-17 18:06:56 +0800

    add hbase instance and hbase configs.

--- dao/utils.py
+++ dao/utils.py
@@ -1,4 +1,8 @@
 # coding=utf8
+'''
+    utils file.
+    just for support functions and connections.
+'''
 
 import pymongo
 import happybase
@@ -7,13 +11,27 @@ from config import MONGODB_HOST
 from config import MONGODB_PORT
 from config import MONGODB_DBNAME
 
+from config import HBASE_HOST
+
 
 def get_db(
     mongo_host=MONGODB_PORT,
     mongo_port=MONGODB_PORT,
     mongo_dbname=MONGODB_DBNAME
 ):
-    
+    '''
+        get a mongodb instance
+    '''
     return pymongo.Connection(MONGODB_HOST, MONGODB_PORT)['sandbox_mongo_5']
 
+
+def get_hbase_instance(hbase_host=HBASE_HOST, autoconnect=True):
+    '''
+        get a hbase instance
+    '''
+    return happybase.Connection(hbase_host, autoconnect=autoconnect)
+
+
 MONGODB_INSTANCE = get_db()
+
+HBASE_INSTANCE = get_hbase_instance()
