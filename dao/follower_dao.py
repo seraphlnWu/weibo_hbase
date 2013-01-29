cd24d65c6a0954645fff87013613f8b6fa86b85c
zhanglei <zhanglei@admaster.com.cn>
2013-01-29 16:45:01 +0800

    move flwr code to follower_dao

--- dao/follower_dao.py
+++ dao/follower_dao.py
@@ -6,6 +6,7 @@ from base import BaseQuery
 
 from utils import MONGODB_INSTANCE
 from utils import HBASE_INSTANCE
+from smdata.tasks import save_cache_flwr_list
 
 FOLLOWER_TB = HBASE_INSTANCE.table('followers')
 FOLLOW_RELATION_TB = HBASE_INSTANCE.table('follow_relations')
@@ -25,7 +26,7 @@ def get_follower_attr(uid, follower_id, attrs):
     """返回针对当前用户的评论数"""
     dao = FollowRelationDao()
     return dao.query_one(*attrs, id='%s_%s' % (uid, follower_id))
-    
+
 def get_cache_flwr_by_page(
     uid,
     sort_type='followers_count', 
@@ -33,9 +34,59 @@ def get_cache_flwr_by_page(
     records_per_page=10
 ):
     """
-    TODO: wait for map reduce job
+    TODO
     """
-    pass
+    
+    page_info = {}
+
+    flwrs = get_flwr_cache(uid, sort_type)
+    stlen = 100 if len(flwrs) > 100 else len(flwrs)
+    flwrs = flwrs[(page-1)*records_per_page: page*records_per_page]
+    
+    if stlen % records_per_page:
+        page_info['page_totals'] = stlen / records_per_page + 1
+    else:
+        page_info['page_totals'] = stlen / records_per_page
+        
+    page = [page, 1][page < 1 or page > page_info['page_totals']]
+    page_info['current_page'] = page
+    page_info['records_per_page'] = records_per_page
+    page_info['pre_page'] = page - 1 if page > 1 else page
+    page_info['next_page'] = page + 1 if page < page_info['page_totals'] else page_info['page_totals']
+    page_info['sort_type'] = sort_type
+
+    result_list = []
+    for flwr in flwrs:
+        fid = flwr[1]
+        tmp_flwr = MONGODB_INSTANCE.follow_relations.find_one({'user_id': uid, 'follower_id': fid})
+        ret = MONGODB_INSTANCE.followers.find_one({'_id': fid})
+        if ret:
+            tmp_flwr.update({
+                'name': ret['name'], 
+                'profile_image_url': ret['profile_image_url'], 
+                'verified': ret.get('verified'),
+                'gender': ret.get('gender'),
+                'location': ret.get('location'),
+                'id': ret.get('_id'),
+                'created_at_': ret.get('created_at'),
+            })
+        if tmp_flwr:
+            tmp_flwr.update({sort_type: flwr[2]})
+            result_list.append((flwr[0], tmp_flwr))
+
+    return page_info, result_list
+    
+def get_flwr_cache(uid, sort_type):
+    """
+    TODO
+    """
+    flwr_cache = MONGODB_INSTANCE.flwr_cache.find_one({'_id': uid}, {sort_type: 1})
+
+    if not flwr_cache or not flwr_cache.get(sort_type):
+        save_cache_flwr_list(uid, keyword_list=[sort_type])
+        flwr_cache = MONGODB_INSTANCE.flwr_cache.find_one({'_id': uid}, {sort_type: 1})
+
+    return flwr_cache.get(sort_type)
 
 def get_followers_by_page(uid, 
     sort_type='all',
